7508cbf68abae351809ec91758afc35cd17c337b
JosePedro <alejandro.manzanares.01@gmail.com>
2019-04-03 17:02:49 +0200

    Actualización prácticas

--- js1/mascotas/js/mascotas.js
+++ js1/mascotas/js/mascotas.js
@@ -1,14 +1,26 @@
 (function(panclaje){
-function generadorMascotas(){
 
-                            //Declaración array que almacenará los nombres
+                                            //Array con varios nombres como valor para su uso en generadorNombres()
+
+        const nombres = ['missy', 'rex', 'dog', 'bob', 'luna', 'max', 'sebes', 'ginna', 'balto', 'taxi'];
+
+    function generadorNombres(){
+
+                                            //variable nmascota adopta valor aleatorio del array nombres
+
+        let nmascota = nombres[Math.floor(Math.random() * nombres.length)];
+
+        return nmascota;
+    }
+    function generadorMascotas(){
+
+                            //Declaración array que almacenará los objetos mascota
     let mascotas = [];
+                            //Bucle asignación valores array
 
-                            //Bucle asignación nombres array
+    for(i = 1; i<=1000; i++){
 
-    for(i = 0; i<1000; i++){
-
-        mascotas[i] = 'Mascota' + (i+1);
+        mascotas[i] = {id:i,nombre: generadorNombres()+ ' ' + generadorNombres(),edad:i,precio:i};
 
     }
                             //Return de array con propiedades asignadas
@@ -27,12 +39,28 @@ function imprimirMascotas(panclaje){
                             //Bucle creación <li></li>
     for(i in mascotas){
 
-        impresion += '<li>' + mascotas[i] + '</li>';
+        impresion += `
+        <li>
+            <p>Identificador: ${mascotas[i].id}</p>
+            <p>Nombre: ${mascotas[i].nombre}</p>
+            <p>Edad: ${mascotas[i].edad}</p>
+            <p>Tasa de adopción: ${mascotas[i].precio}€</p>
+            <span><button class='btn-comprar' data-id="${mascotas[i].id}">¡Adoptar!</button></span>
+        </li>
+        `;
+        //'<li>Identificador mascota: ' + mascotas[i].id + ', nombre: ' + mascotas[i].nombre +', edad: ' + mascotas[i].edad + ', tasa de adopción: ' + mascotas[i].precio + '€</li><br />';
     }
                             //Impresión en pantalla de resultado bucle
 
     panclaje.innerHTML = impresion;
-};
 
+    document.querySelectorAll('.btn-comprar').forEach(function(btn){
+        const id = btn.dataset.id;
+        btn.onclick = function(){alert('Añadido ' + id + ' al proceso de adopción.')};
+
+
+    });
+};
+                            //Llamada a función al abrir ventana
 imprimirMascotas(panclaje);
 })(document.body.children[0].children[0]);
